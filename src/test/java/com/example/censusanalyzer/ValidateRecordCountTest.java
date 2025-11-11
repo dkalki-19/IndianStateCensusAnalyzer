461c7ed6e9066ae8cca2bba4dd94e90aa775c1e9
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-11-11 12:24:37 +0530

    UC2_TC2_2_IncorrectFile

---
+++ src/test/java/com/example/censusanalyzer/ValidateRecordCountTest.java
@@ -0,0 +1,14 @@
+package com.example.censusanalyzer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ValidateRecordCountTest {
+
+	@Test
+    public void givenStateCodeCSV_WhenCorrect_ShouldReturnCorrectRecords() throws Exception {
+        StateCensusAnalyser analyser = new StateCensusAnalyser();
+        int count = analyser.loadStateCode("./IndianStateCode.csv");
+        assertEquals(37, count); // Use your actual number from CSV
+    }
+	
+}
